3761e89b3ad82d6065d7e21e420af7c734e0cb32
nick adams <thebigdeal916@icloud.com>
2020-07-26 14:47:24 -0700

    btn-updates

--- index.js
+++ index.js
@@ -4,14 +4,20 @@ const bubba = document.querySelector("#bubba-btn")
 const plat = document.querySelector("#platinum-btn")
 
 // Adding simple event listeners to our buttons
+// These are for a demonstration for now, more 
+// functionality will be added soon.
+
+// GDP btn functionality.
 gdp.addEventListener("click", function() {
     alert("just ordered some fire ass GDP!")
 })
 
+// Bubba kush btn functionality.
 bubba.addEventListener("click", function() {
     alert("some of dat bubba comin your way!")
 })
 
+// Platinum btn functionality.
 plat.addEventListener("click", function() {
     alert("Our finest Platinum is en route")
 })
