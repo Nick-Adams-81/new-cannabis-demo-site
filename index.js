b4026af190506a3f3c47c6327ee6d04bf255d282
nick adams <thebigdeal916@icloud.com>
2020-07-26 15:21:45 -0700

    gitignore added

--- index.js
+++ index.js
@@ -3,21 +3,28 @@ const gdp = document.querySelector("#gdp-btn")
 const bubba = document.querySelector("#bubba-btn")
 const plat = document.querySelector("#platinum-btn")
 
-// Adding simple event listeners to our buttons
+// Adding simple event listeners to our buttons.
 // These are for a demonstration for now, more 
 // functionality will be added soon.
 
 // GDP btn functionality.
 gdp.addEventListener("click", function() {
-    alert("just ordered some fire ass GDP!")
+    alert("GDP added to cart")
 })
 
 // Bubba kush btn functionality.
 bubba.addEventListener("click", function() {
-    alert("some of dat bubba comin your way!")
+    alert("Bubba kush added to cart")
 })
 
 // Platinum btn functionality.
 plat.addEventListener("click", function() {
-    alert("Our finest Platinum is en route")
+    Swal.fire(
+        'Platinum OG added to cart!',
+        'Weed on the way',
+        'success'
+      )
+
+      //imageUrl: "https://darkheartnursery.com/wp-content/uploads/2014/07/lf_platinum_OG_9633.jpg",
+     
 })
