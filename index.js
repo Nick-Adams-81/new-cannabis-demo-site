50f0ace2ff3ce4cd38255338ffa03fdb2285bfdb
nick adams <thebigdeal916@icloud.com>
2020-07-26 14:44:35 -0700

    bun update

---
+++ index.js
@@ -0,0 +1,17 @@
+// declaring our global variables
+const gdp = document.querySelector("#gdp-btn")
+const bubba = document.querySelector("#bubba-btn")
+const plat = document.querySelector("#platinum-btn")
+
+// Adding simple event listeners to our buttons
+gdp.addEventListener("click", function() {
+    alert("just ordered some fire ass GDP!")
+})
+
+bubba.addEventListener("click", function() {
+    alert("some of dat bubba comin your way!")
+})
+
+plat.addEventListener("click", function() {
+    alert("Our finest Platinum is en route")
+})
